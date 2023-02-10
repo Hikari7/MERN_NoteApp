59b495ff292ce8a39316e96d7ad9fb27db6fa79b
Hikari7 <h.kobe712@gmail.com>
2023-02-10 15:17:51 -0800

    imported jwt

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const CryptoJS = require("crypto-js");
+const JWT = require("jsonwebtoken");
+const User = require("./src/v1/models/user");
+
 const app = express();
-const PORT = 8000;
 const dotenv = require("dotenv").config();
 
 //Connect to the DB
@@ -12,10 +15,29 @@ try {
   console.log(err);
 }
 
+app.post("/register", async (req, res) => {
+  //receive the pw
+  const password = req.body.password;
+  try {
+    //pwの暗号化
+    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+    //ユーザーの新規作成(DBへ保存)
+    const user = await User.create(req.body);
+    //JWTの発行
+    //各ユーザーごとに割り振られたMongoDBでに保存されているUserIDをもとにJWTを発行していく
+    const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+      expiresIn: "24h",
+    });
+    return res.status(200).json({ user, token });
+  } catch(err) {
+    return res.status(500).json(err)
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello express");
 });
 
-app.listen(PORT, () => {
-  console.log("listening server now🚀");
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Server has started...🚀");
 });
