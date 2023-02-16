30255e04b71407b53bfdeee24f85f6ed2ebf2b32
Hikari7 <h.kobe712@gmail.com>
2023-02-16 10:53:58 -0800

    added validation UI for the register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,11 +3,19 @@ import React from "react";
 import { LoadingButton } from "@mui/lab";
 import { Link } from "react-router-dom";
 import authApi from "../api/authApi";
+import { useState } from "react";
 
 const Register = () => {
-    
+  const [usernameErrText, setUsernameErrText] = useState("");
+  const [passwordErrText, setPasswordErrText] = useState("");
+  const [confirmErrText, setConfirmErrText] = useState("");
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setUsernameErrText("");
+    setPasswordErrText("");
+    setConfirmErrText("");
+
     //入力欄の文字列を取得
     const data = new FormData(e.target);
 
@@ -20,6 +28,28 @@ const Register = () => {
     console.log(password);
     console.log(confirmPassword);
 
+    let error = false;
+
+    if (username === "") {
+      error = true;
+      setUsernameErrText("Please enter a name");
+    }
+    if (password === "") {
+      error = true;
+      setPasswordErrText("Please enter a password");
+    }
+    if (confirmPassword === "") {
+      error = true;
+      setConfirmErrText("Please enter a password");
+    }
+    if (password !== confirmPassword) {
+      error = true;
+      setConfirmErrText("password doesn't match");
+    }
+
+    //errorだったら以下の処理はせずに返しちゃう
+    if (error) return;
+
     //新規登録APIを叩く
     try {
       //suthApiのparamsをここでいれる(bodyに挿入される→serverに渡されて暗号化されたり)
@@ -29,20 +59,22 @@ const Register = () => {
         password,
         confirmPassword,
       });
+
       //レスポンスとして帰ってきたトークン属性をローカルストレージに保存
       //サーバーのresの中のtokenを入れる
       localStorage.setItem("token", res.token);
       console.log("success!");
+      
     } catch (err) {
       console.log(err);
-     const errors = err.data.errors;
-      console.log(errors);
+      // const errors = err.data.errors;
+      // console.log(errors);
     }
   };
 
   return (
     <>
-      <Box component="form" onSubmit={handleSubmit}>
+      <Box component="form" onSubmit={handleSubmit} noValidate>
         <TextField
           fullWidth
           id="username"
@@ -50,6 +82,8 @@ const Register = () => {
           margin="normal"
           name="username"
           required
+          helperText={usernameErrText}
+          error={usernameErrText !== ""}
         />
         <TextField
           fullWidth
@@ -59,6 +93,8 @@ const Register = () => {
           name="password"
           type="password"
           required
+          helperText={passwordErrText}
+          error={passwordErrText !== ""}
         />
         <TextField
           fullWidth
@@ -68,6 +104,8 @@ const Register = () => {
           name="confirmPassword"
           type="password"
           required
+          helperText={confirmErrText}
+          error={confirmErrText !== ""}
         />
         <LoadingButton
           sx={{ mt: 3, mb: 2, textTransform: "none" }}
